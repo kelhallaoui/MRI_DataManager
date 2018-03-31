f216671835492c8d1358c83df361b3605a279a0e
Yip Sang Leung <thirtythousandollar@gmail.com>
2018-03-31 14:48:34 +0800

    Update DataManager to support BRATS

--- DataManager/DataManager.py
+++ DataManager/DataManager.py
@@ -2,13 +2,20 @@ import os
 import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
-from Utilities.utilities import extract_NIFTI, read_CSV, write_data, get_FigShare_filemap
+from Utilities.utilities import (
+	extract_NIFTI,
+	read_CSV,
+	write_data,
+	get_FigShare_filemap,
+	get_BRRATS_filemap,
+)
 from DataManager.PreProcessData import *
 from DataManager.FeatureExtractor import *
 import numpy as np
 
 ADNI = 'ADNI'
 FIG_SHARE = 'FigShare'
+BRATS = 'BRATS'
 
 class DataManager(object):
 	""" Organization of the data
@@ -41,7 +48,7 @@ class DataManager(object):
 			                      	  num_slices: total number of MRI slices
 	"""
 
-	supported_datasets = [ADNI, FIG_SHARE]
+	supported_datasets = [ADNI, FIG_SHARE, BRATS]
 
 	def __init__(self, filepath, datasets = None):
 		for dataset in datasets:
@@ -61,6 +68,10 @@ class DataManager(object):
 			FIG_SHARE: {
 				'data_filepath': 	 filepath + r'/1512427/',
 				'key': 				 'Patient ID'
+				},
+			BRATS: {
+				'data_filepath': 	 filepath + r'/BRATS/',
+				'key': 				 'Subject'
 				}
 			}
 
@@ -84,6 +95,9 @@ class DataManager(object):
 				pid_slice_files_map = get_FigShare_filemap(
 										self.information[dataset]['data_filepath'])
 				self.dataCollection.update({str(dataset): pid_slice_files_map})
+			elif dataset == BRATS:
+				subject_id_files_map = get_BRRATS_filemap(self.information[dataset]['data_filepath'])
+				self.dataCollection.update({str(dataset): subject_id_files_map})
 
 			key = self.information[dataset]['key']
 			self.train_validate_test_split(dataset, column_header=key)
